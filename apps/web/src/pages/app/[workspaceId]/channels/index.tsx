0898b231e5a823822de7d88c41b1a8275a2b0f58
Simon Maribo <simonmaribo@outlook.dk>
2024-07-17 15:32:53 +0200

    Empty state

--- apps/web/src/pages/app/[workspaceId]/channels/index.tsx
+++ apps/web/src/pages/app/[workspaceId]/channels/index.tsx
@@ -12,7 +12,7 @@ import useWorkspace from '@/hooks/use-workspace'
 import getChannelList from '@/queries/workspace/channels/getChannelList'
 import NiceModal from '@ebay/nice-modal-react'
 import { useQuery } from '@tanstack/react-query'
-import { LucideCircleHelp, Wifi } from 'lucide-react'
+import { LucideCircleHelp, Plus, Wifi } from 'lucide-react'
 import { HiQuestionMarkCircle } from 'react-icons/hi'
 
 function ChannelsList() {
@@ -56,11 +56,12 @@ function ChannelsList() {
 						</div>
 						<Button
 							variant="secondary"
-							size="sm"
-							before={<Wifi />}
 							onClick={() => NiceModal.show(CreateChannelModal)}
 						>
-							Create Channel
+							<div className="flex items-center gap-2">
+								<Plus size={16} />
+								<span>Create channel</span>
+							</div>
 						</Button>
 					</div>
 					<div className="mt-4">
@@ -68,7 +69,7 @@ function ChannelsList() {
 							<div className="h-32 flex items-center justify-center">
 								<Loading size="sm" />
 							</div>
-						) : isError ? (
+						) : isError || !data ? (
 							<div>
 								<Alert
 									color="error"
@@ -79,21 +80,52 @@ function ChannelsList() {
 								</Alert>
 							</div>
 						) : (
-							<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-								{data?.map((channel) => (
-									<ChannelCard
-										key={channel.id}
-										id={channel.id}
-										name={channel.name}
-										devices={channel.devices}
-										messages={channel.messages}
-										lastMessageDate={
-											channel.lastMessageDate
-										}
-									/>
-								))}
-								<NewChannelCard />
-							</div>
+							<>
+								{data?.length > 0 ? (
+									<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+										{data?.map((channel) => (
+											<ChannelCard
+												key={channel.id}
+												id={channel.id}
+												name={channel.name}
+												devices={channel.devices}
+												messages={channel.messages}
+												lastMessageDate={
+													channel.lastMessageDate
+												}
+											/>
+										))}
+									</div>
+								) : (
+									<div className="flex items-center justify-center text-center flex-col border h-64 border-dashed rounded-xl bg-neutral-100">
+										<div className="mx-auto max-w-sm">
+											<h1 className="text-lg font-semibold text-gray-800">
+												You have not created any
+												channels yet
+											</h1>
+											<p className="text-sm font-normal text-gray-600">
+												Once you create a channel, you
+												will be able to send real-time
+												push messages to your devices.
+											</p>
+											<Button
+												variant="secondary"
+												className="mt-8"
+												onClick={() =>
+													NiceModal.show(
+														CreateChannelModal
+													)
+												}
+											>
+												<div className="flex items-center gap-2">
+													<Plus size={16} />
+													<span>Create channel</span>
+												</div>
+											</Button>
+										</div>
+									</div>
+								)}
+							</>
 						)}
 					</div>
 				</div>
