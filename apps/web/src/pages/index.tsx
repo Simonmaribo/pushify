24ed6bd7c7766ab5cf6b117908ab430c2724badf
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-07-22 17:05:27 +0200

    pushify

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,18 +1,40 @@
 import Button from '@/components/ui/Button'
+import Link from 'next/link'
 
 function Home() {
 	return (
-		<div className="h-screen flex flex-col gap-2 items-center justify-center">
-			<img
-				src="/logos/logo.png"
-				alt="Pushify Logo"
-				className="w-[250px]"
-			/>
-			<h1 className="text-2xl font-semibold">
-				Send notifications with ease
-			</h1>
-			<Button>Sign up for Pushify</Button>
-		</div>
+		<>
+			<nav className='fixed h-20 flex items-center w-full bg-white'>
+				<div className='w-[86%] mx-auto max-w-7xl'>
+					<img
+						src="/logos/logo.png"
+						alt="Pushify Logo"
+						className="w-[150px]"
+					/>
+				</div>
+			</nav>
+			<section className='pt-20'>
+				<div style={{background: "linear-gradient(90deg, rgba(25,2,64,1) 0%, rgba(48,15,102,1) 100%)"}} className='w-[86%] mx-auto max-w-7xl p-20 gap-32 lg:gap-10 grid grid-cols-1 lg:grid-cols-2 rounded-3xl text-white min-h-[700px]'>
+					<div className='flex flex-col gap-6 justify-center'>
+						<h2 className='text-5xl tracking-tighter font-semibold'>
+							Send fast push notifications <span className='rotate-45'>with ease</span>
+						</h2>
+						<p className='text-gray-50 max-w-md text-lg'>Pushify is a digital service that allows you to send fast push notifications with ease. Get started today.</p>
+						<div className='mt-8'>
+							<Link className='border-2 text-sm px-5 py-2 rounded-full transition-all hover:bg-white hover:text-blue-900' href={"/get-started"}>
+								Get started
+							</Link>
+						</div>
+					</div>
+					<div className='flex flex-col justify-center items-center'>
+						<div className='flex items-center'>
+							<img className='w-[200px] mr-[-70px]' src="/img/phone.svg"></img>
+							<img className='w-[220px]' src="/img/phone.svg"></img>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
 	)
 	/*return (
 		<div className="p-4">
