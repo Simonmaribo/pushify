ecd7a51cbe821686e0b301d80a48a7256137be33
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-07-25 22:03:25 +0200

    new

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import SectionIntro from '@/components/SectionIntro'
 import Button from '@/components/ui/Button'
+import { Github, GithubIcon } from 'lucide-react'
 import Link from 'next/link'
+import { FaGithub } from 'react-icons/fa'
 
 function Home() {
 	return (
@@ -54,12 +56,39 @@ function Home() {
 					</div>
 				</div>
 			</section>
-			<SectionIntro heading='How it works'>
-				<p>hey</p>
-			</SectionIntro>
-			<SectionIntro heading="We're proudly open-source">
-				<p>hey</p>
-			</SectionIntro>
+			<section className="pb-24">
+				<div className="w-[86%] flex flex-col gap-6 mx-auto max-w-[930px]">
+					<h1 className="font-semibold tracking-tighter max-w-xl text-5xl">How it works</h1>
+					<p className="text-xl max-w-lg text-gray-600">
+						We have made it easy to integrate and track your important events in
+						just a few minutes.
+					</p>
+				</div>
+        	</section>
+			<section className="py-24 border-t border-gray-600/10">
+				<div className="w-[86%] flex flex-col gap-6 mx-auto max-w-[930px]">
+					<div className='flex mb-4'>
+						<Link href={"/"} className='flex items-center gap-2 shadow-sm rounded-full border border-gray-600/10 px-3 py-2 hover:bg-[#fafafa] transition-all'>
+							<FaGithub className='w-6 h-6' />
+							<p>Visit us on Github</p>
+						</Link>
+					</div>
+					<h1 className="font-semibold tracking-tighter max-w-xl text-5xl">We're proudly open-source</h1>
+					<p className="text-xl max-w-lg text-gray-600">
+						We have made it easy to integrate and track your important events in
+						just a few minutes.
+					</p>
+				</div>
+        	</section>
+			<section className="py-24 border-t border-gray-600/10">
+				<div className="w-[86%] flex flex-col gap-6 mx-auto max-w-[930px]">
+					<h1 className="font-semibold tracking-tighter max-w-xl text-5xl">Pricing for everyone</h1>
+					<p className="text-xl max-w-lg text-gray-600">
+						We have made it easy to integrate and track your important events in
+						just a few minutes.
+					</p>
+				</div>
+        	</section>
 			<section className='py-24 border-t border-gray-600/10'>
 				<div className='w-[86%] mx-auto max-w-[930px] gap-32 lg:gap-10 rounded-3xl text-black'>
 					<div className='flex flex-col gap-6 justify-center'>
