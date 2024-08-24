e9769f64fe101288051b501dfc243926c8c6c78b
Simon Maribo <simonmaribo@outlook.dk>
2024-08-24 14:10:07 +0200

    F

---
+++ apps/api/src/routes/v1/integrations/ifttt/v1/actions/send_push_notification.ts
@@ -0,0 +1,58 @@
+import type Server from '../../../../../../interfaces/Server'
+import type { Request, Response, Router } from 'express'
+import * as z from 'zod'
+
+const router = require('express').Router({ mergeParams: true }) as Router
+
+module.exports = (server: Server) => {
+	return {
+		router: () => {
+			router.get('/', async (req: Request, res: Response) => {
+				const channelKey = req.headers['ifttt-channel-key']
+				const serviceKey = req.headers['ifttt-service-key']
+				if (
+					!channelKey ||
+					!serviceKey ||
+					serviceKey !== process.env.IFTTT_SERVICE_KEY
+				) {
+					return res.status(401).json({
+						error: 'Unauthorized',
+					})
+				}
+
+				const schema = z.object({
+					api_key: z.string(),
+					channel_id: z.string(),
+					title: z.string(),
+					message: z
+						.string()
+						.optional()
+						.or(z.undefined())
+						.or(z.null())
+						.or(z.literal('')),
+					url: z
+						.string()
+						.url()
+						.optional()
+						.or(z.undefined())
+						.or(z.null())
+						.or(z.literal('')),
+				})
+
+				const { actionFields } = req.body
+
+				try {
+					const data = schema.parse(actionFields)
+				} catch (error) {
+					return res.status(400).json({
+						error: 'Action fields are invalid',
+					})
+				}
+
+				return res.json({ message: 'Push Message Sent' })
+			})
+
+			return router
+		},
+	}
+}
